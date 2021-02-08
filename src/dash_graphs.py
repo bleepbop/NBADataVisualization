b2a8eb7e888e51613d4375b6debcb54620f9c2d2
bleepbop <benlee117@gmail.com>
2021-02-08 18:49:08 -0500

    add requirements.txt, reformat graph layout

---
+++ src/dash_graphs.py
@@ -0,0 +1,108 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
+import plotly.express as px
+
+from data_init import *
+
+NBA_TEAMS = get_teams()
+# Initial team will be Philly
+philly = teams.find_teams_by_city('Philadelphia')[0]['id']
+combined_data = init_dataframe(philly)
+
+# Create initial figure w/ default parameters.
+x_value = "E_DEF_RATING"
+y_value = "DEFLECTIONS"
+nba_fig = px.scatter(combined_data, x=x_value, y=y_value, size='W_PCT', color='PLAYER_NAME')
+fantasy_df = create_fantasy_df()
+fantasy_fig = px.scatter(fantasy_df,
+                         x='ADP',
+                         y='Fantasy Average Per Game',
+                         labels={
+                             "Player": "Player Name",
+                             "ADP": "Average Draft Position",
+                             "Fantasy Average Per Game": "Average Fantasy Points Per Game"
+                         },
+                         hover_name="Player")
+
+# Create figure controls.
+controls = dbc.Card(
+    [
+        dbc.FormGroup(
+            [
+                dbc.Label('Select Team'),
+                dcc.Dropdown(
+                    id="dropdown-nba-team",
+                    options=[{'label': team['full_name'], 'value': team['id']} for team in NBA_TEAMS],
+                    placeholder="Insert team name",
+                    persistence=True
+                ),
+            ]
+        ),
+        dbc.FormGroup(
+            [
+                dbc.Label('X Variable'),
+                dcc.Dropdown(
+                        id='x-coord-dropdown',
+                        options=[{'label': i, 'value': columns_dict[i]} for i in columns_dict],
+                        placeholder='Select a X Coordinate Parameter',
+                        persistence=True
+                        )
+            ]
+        ),
+        dbc.FormGroup(
+            [
+                dbc.Label('Y Variable'),
+                dcc.Dropdown(
+                        id='y-coord-dropdown',
+                        options=[{'label': i, 'value': impact_stats_dict[i]} for i in impact_stats_dict],
+                        placeholder='Select a Y Coordinate Parameter',
+                        persistence=True
+                        )
+            ]
+        )
+    ],
+    body=True
+)
+
+# Create the app
+app = dash.Dash(
+    external_stylesheets=[dbc.themes.BOOTSTRAP]
+)
+
+# Create the general app layout
+app.layout = dbc.Container(
+    children=[
+        html.H1(children='NBA Data Visualizer', style={'textAlign': 'center'}),
+        html.Hr(),
+        html.H3(children='Hustle Stats Plots', style={'textAlign': 'center'}),
+        dbc.Row(
+            [
+                dbc.Col(controls, md=4),
+                dbc.Col(dcc.Graph(id='nba-scatter-plot', figure=nba_fig), md=8),
+            ],
+            align="center",
+        ),
+        html.Hr(),
+        html.H3(children='Fantasy Value per ADP', style={'textAlign': 'center'}),
+        dbc.Row(
+            [dbc.Col(dcc.Graph(id='fantasy-adp-plot', figure=fantasy_fig))]
+        )
+    ],
+    fluid=True,
+)
+
+@app.callback(
+    Output(component_id='nba-scatter-plot', component_property='figure'),
+    Input(component_id='x-coord-dropdown', component_property='value'),
+    Input(component_id='y-coord-dropdown', component_property='value'),
+    Input(component_id='dropdown-nba-team', component_property='value'),
+)
+# This callback method takes in control parameters and updates the plot accordingly.
+def update_scatter(input_x_value, input_y_value, team_id):
+    dataset = init_dataframe(team_id)
+    return px.scatter(dataset, x=input_x_value, y=input_y_value, size='W_PCT', color='PLAYER_NAME')
+
+app.run_server(debug=True, use_reloader=True)
---
+++ src/data_init.py
@@ -0,0 +1,122 @@
+from nba_api.stats.endpoints import (teamplayeronoffdetails,
+                                     leaguehustlestatsplayer,
+                                     teamplayerdashboard,
+                                     playerestimatedmetrics,
+                                     fantasywidget)
+from nba_api.stats.static import teams, players
+import pandas as pd
+from pandas import DataFrame
+
+
+columns_dict = {"Contested Shots": "CONTESTED_SHOTS",
+                "Contested 2 Pt Shots": "CONTESTED_SHOTS_2PT",
+                "Contested 3 Pt Shots": "CONTESTED_SHOTS_3PT",
+                "Deflections": "DEFLECTIONS",
+                "Charges Drawn": "CHARGES_DRAWN",
+                "Screen Assists": "SCREEN_ASSISTS",
+                "Screen Assist Pts": "SCREEN_AST_PTS",
+                "Offensive Loose Balls Recovered": "OFF_LOOSE_BALLS_RECOVERED",
+                "Defensive Loose Balls Recovered": "DEF_LOOSE_BALLS_RECOVERED",
+                "Loose Balls Recovered": "LOOSE_BALLS_RECOVERED",
+                "Offensive Boxouts": "OFF_BOXOUTS",
+                "Defensive Boxouts": "DEF_BOXOUTS"
+                }
+
+impact_stats_dict = {"Offensive Rating": 'E_OFF_RATING',
+                     "Defensive Rating": 'E_DEF_RATING',
+                     "Net Rating": 'E_NET_RATING',
+                     "Win Percent": 'W_PCT'}
+
+def get_fantasy_adp_df():
+    # All credit for ADP goes to CBS Sports.
+    url = 'https://www.cbssports.com/fantasy/basketball/draft/averages/'
+    df = pd.read_html(url)[0]
+    fantasy_adp_df = df[['Player', 'Avg Pos']]
+    return fantasy_adp_df
+
+def create_fantasy_df():
+    adp_df = get_fantasy_adp_df()
+    total_fantasy_players = adp_df['Player']
+    player_id_df = create_active_players_df()
+    fantasy_data_df = fantasywidget.FantasyWidget().get_data_frames()[0]
+    dfs_list = []
+
+    for index, row in player_id_df.iterrows():
+        player = row['Player']
+        pid = row['id']
+
+        # Continue if we don't have ADP data for this player.
+        if not adp_df['Player'].str.contains(player).sum():
+            continue
+        player_df = adp_df[adp_df['Player'].str.contains(player)]
+        player_adp = float(player_df['Avg Pos'])
+
+        player_fantasy_data = fantasy_data_df.loc[fantasy_data_df['PLAYER_ID'] == pid].copy()
+        fantasy_avgs = (player_fantasy_data['NBA_FANTASY_PTS'])
+
+        if type(fantasy_avgs) == pd.core.series.Series:
+            if fantasy_avgs.size > 1:
+                fantasy_avgs = fantasy_avgs.mean()
+            elif fantasy_avgs.size == 0:
+                fantasy_avgs = 0
+            else:
+                fantasy_avgs = fantasy_avgs.values
+        fantasy_avgs = float(fantasy_avgs)
+        new_df = DataFrame(data=[[player, player_adp, fantasy_avgs]], columns=['Player', 'ADP', 'Fantasy Average Per Game'])
+        dfs_list.append(new_df)
+    # Create single df containing individual player fantasy data
+    combined_fantasy_data_df = pd.concat(dfs_list)
+    return combined_fantasy_data_df
+
+def create_active_players_df():
+    all_players = players.get_active_players()
+    df = pd.DataFrame(all_players)
+    df.rename(columns = {'full_name':'Player'}, inplace = True)
+    pid_df = df[['id', 'Player']]
+    return pid_df
+
+def get_teams():
+    team_info = teams.get_teams()
+    return team_info
+
+def init_dataframe(team_id):
+    metrics = playerestimatedmetrics.PlayerEstimatedMetrics().get_data_frames()[0]
+
+    '''
+    info = teamplayeronoffdetails.TeamPlayerOnOffDetails(team_id=philly['id'])
+    players_on = info.get_data_frames()[1]
+    players_off = info.get_data_frames()[2]
+    '''
+
+    # Get player names for a given team
+    team = teamplayerdashboard.TeamPlayerDashboard(team_id=team_id)
+    team_df = team.get_data_frames()[1]
+    roster = team_df[['PLAYER_ID', 'PLAYER_NAME']]
+
+    hustle_stats = leaguehustlestatsplayer.LeagueHustleStatsPlayer()
+    hustle_df = hustle_stats.get_data_frames()[0]
+
+    # Remove unnecessary columns
+    del hustle_df['PCT_BOX_OUTS_OFF']
+    del hustle_df['PCT_BOX_OUTS_DEF']
+
+    # Columns to normalize:
+    cols = columns_dict.values()
+
+    team_metrics = []
+    df_list = []
+    for player_id in roster['PLAYER_ID']:
+        player_df = hustle_df.loc[hustle_df['PLAYER_ID'] == player_id].copy()
+        games_played = player_df['G']
+        # Normalize Data to per game stats.
+        for col in cols:
+            player_df[col] = player_df[col] / games_played
+        df_list.append(player_df)
+        player_metrics_df = metrics.loc[metrics['PLAYER_ID'] == player_id]
+        team_metrics.append(player_metrics_df[["PLAYER_ID", "E_OFF_RATING", "E_DEF_RATING", "E_NET_RATING", "W_PCT"]])
+
+    team_hustle_df = pd.concat(df_list)
+    player_metrics_df = pd.concat(team_metrics)
+    # Merge dataframes on player name.
+    merged_df = pd.merge(team_hustle_df, player_metrics_df, on='PLAYER_ID')
+    return merged_df
